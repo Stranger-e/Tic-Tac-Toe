af08ac2ae3272789bab617d4fabe4d7c2c5e1d16
Ezekiel OLoo <olooezekiel885@gmail.com>
2024-04-11 15:37:22 +0300

    Add a create board and reset board logic

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log('It is working properly')
+const createGameboard = (() => {
+    let gameboard = ['','','','','','','','',''];
+
+    const displayGameboard = () => {
+        console.log('New Board');
+        for (let i = 0; i < 9; i += 3) {
+            console.log(board.slice(i, i + 3).join(' | '));
+            if (i < 6) console.log('---------');
+        }
+    };
+
+    const resetGameboard = () => {
+        gameboard = ['','','','','','','','',''];
+    };
+
+    return {displayGameboard, resetGameboard};
+})();
