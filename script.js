32638f34a925234a488c7bde492e20a518f1e4ef
Ezekiel OLoo <olooezekiel885@gmail.com>
2024-04-11 16:01:41 +0300

    Add a create player factory function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const createGameboard = (() => {
     const displayGameboard = () => {
         console.log('New Board');
         for (let i = 0; i < 9; i += 3) {
-            console.log(board.slice(i, i + 3).join(' | '));
+            console.log(gameboard.slice(i, i + 3).join(' | '));
             if (i < 6) console.log('---------');
         }
     };
@@ -15,3 +15,17 @@ const createGameboard = (() => {
 
     return {displayGameboard, resetGameboard};
 })();
+
+const Player = (name, symbol) => {
+    return{name, symbol};
+}
+
+let player1 = Player('Oloo', 'O');
+let player2 = Player('Max', 'X')
+console.log(player1);
+console.log(player2);
+
+const gameFlow = (() => {
+    const players = [];
+    let currentPlayerIndex = 0;
+});
