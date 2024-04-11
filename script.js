832cf355f4ea419e1ac21bc4927e54a1203f29f4
Ezekiel OLoo <olooezekiel885@gmail.com>
2024-04-11 16:15:07 +0300

    Add some gameflow logic in the game for now

--- script.js
+++ script.js
@@ -21,11 +21,26 @@ const Player = (name, symbol) => {
 }
 
 let player1 = Player('Oloo', 'O');
-let player2 = Player('Max', 'X')
+let player2 = Player('Max', 'X');
 console.log(player1);
 console.log(player2);
 
 const gameFlow = (() => {
     const players = [];
     let currentPlayerIndex = 0;
+
+    const switchPlayer = () => {
+        currentPlayerIndex = (currentPlayerIndex === 0) ? 1 : 0;
+    };
+
+    const addPlayer = (player) => {
+        players.push(player);
+    };
+
+    const startGame = () => {
+        createGameboard.resetGameboard();
+        createGameboard.displayGameboard();
+    }
+
+    return {addPlayer, startGame};
 });
